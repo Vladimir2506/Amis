83f4277563f382855938ef2b8f1d94cb3359710f
夏卓凡 <xiazhuofan@qq.com>
2018-12-27 15:49:56 +0800

    添加项目文件。

---
+++ Amis/CSModule.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+
+namespace Amis
+{
+    class CSModule
+    {
+        private IPEndPoint serverEndPoint = null;
+        private Socket socketToServer = null;
+
+        public CSModule(string ipAddress, int portNO)
+        {
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), portNO);
+            socketToServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socketToServer.Connect(serverEndPoint);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "发起连接错误");
+            }
+        }
+
+        public void Release()
+        {
+            try
+            {
+                socketToServer.Disconnect(true);
+                socketToServer.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "断开连接错误");
+            }
+        }
+
+        public string QueryOnce(string msg)
+        {
+            string recv = "NRP";
+
+            try
+            {
+                byte[] toSend = Encoding.UTF8.GetBytes(msg);
+                socketToServer.Send(toSend);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "发送错误");
+                return recv;
+            }
+
+            try
+            {
+                byte[] buffer = new byte[2 * 1024 * 1024];
+                int recvLength = socketToServer.Receive(buffer);
+                recv = Encoding.UTF8.GetString(buffer, 0, recvLength);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString(), "接收错误");
+                return recv;
+            }
+
+            return recv;
+        }
+    }
+}
---
+++ Amis/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Amis
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CSModule connectionToServer = null;
+        private string result = null;
+        private string userID = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AmisLogin_Loaded(object sender, RoutedEventArgs e)
+        {
+            connectionToServer = new CSModule("166.111.140.14", 8000);
+        }
+
+        private void AmisLogin_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            result = connectionToServer.QueryOnce("logout"+userID);
+            Console.WriteLine(result);
+            connectionToServer.Release();
+        }
+    }
+}
